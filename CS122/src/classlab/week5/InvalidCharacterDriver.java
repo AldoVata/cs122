fc5abd1a8ae6004248ab0cf6e926639868369e78
AldoVata <aldoigli12@gmail.com>
2021-10-04 14:16:21 -0400

    InvalidCharacterException class work
    Contains InvalidCharacterException and InvalidCharacterDriver classes

---
+++ CS122/src/classlab/week5/InvalidCharacterDriver.java
@@ -0,0 +1,32 @@
+package classlab.week5;
+import java.util.Scanner;
+
+public class InvalidCharacterDriver {
+
+	public static void main(String[] args){
+		String input;
+		
+		Scanner scan = new Scanner(System.in);
+
+		System.out.print("Enter a Character (000 to quit): ");
+		input = scan.nextLine();
+		while(!input.equals("000"))
+		{
+			try {
+				if(input.charAt(0) <= 90 && input.charAt(0) >= 65)
+					System.out.println("Valid Character");
+				else
+					throw new InvalidCharacterException("Not a valid character");
+			}catch(IndexOutOfBoundsException e)
+			{
+				System.out.println("Input is Empty");
+			}catch(InvalidCharacterException e)
+			{
+				System.out.println(e.getMessage());
+			}
+			System.out.print("Enter a Character (000 to quit): ");
+			input = scan.nextLine();
+		}
+	}
+
+}
---
+++ CS122/src/classlab/week5/InvalidCharacterException.java
@@ -0,0 +1,8 @@
+package classlab.week5;
+
+public class InvalidCharacterException extends Exception{
+	InvalidCharacterException(String message)
+	{
+		super(message);
+	}
+}
