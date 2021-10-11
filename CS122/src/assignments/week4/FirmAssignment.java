a167c83d1085dfb9a55a5b393aa74912b32be5dc
AldoVata <aldoigli12@gmail.com>
2021-10-11 17:46:15 -0400

    Assignment Week 4

---
+++ CS122/src/assignments/week4/FirmAssignment.java
@@ -0,0 +1,11 @@
+package assignments.week4;
+
+import classSamples.polymorphism.Firm.Staff;
+
+public class FirmAssignment {
+	public static void main(String[] args) {
+		Staff personnel = new Staff();
+
+		personnel.payday();
+}
+}
