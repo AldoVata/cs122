4aabf1134463ca221adde7ae71f63835897b2c75
dhruv857 <dgandhi@pace.edu>
2021-11-17 12:56:53 -0500

    class 11/17

--- CS122/src/classlab/week12/StacksQueuesDriver.java
+++ CS122/src/classlab/week12/StacksQueuesDriver.java
@@ -1,5 +1,8 @@
 package classlab.week12;
 
 public class StacksQueuesDriver {
-
+	public static void main(String[] args) {
+		// create a stack of int, push 5 items, pop 3, print at each step
+		// create a queue of string, enqueue 3 items, dequeue 2, print at each stepc
+	}
 }
